419894d3e52aa36b2f0008f0c262a59f30be8b40
Ankush Singhal <ankushsinghal1995@gmail.com>
2019-05-12 14:28:18 +0530

    Basic Reddit bot to fetch comments from a subreddit
    1) The basic bot code to fetch 25 latest comments from the subreddit
    "reddevils"

---
+++ iamatestingbot.py
@@ -0,0 +1,24 @@
+import praw
+
+subreddit_name = "reddevils"
+num_comments = 25
+
+def authenticate():
+	print("Authenticating...")
+	reddit = praw.Reddit("iamatestingbot", user_agent = "Testing Reddit Bot")
+	print("Authenticated as {}".format(reddit.user.me()))
+	return reddit
+
+def run_bot(reddit):
+	print("Obtaining 25 comments")
+	for comment in reddit.subreddit(subreddit_name).comments(limit = num_comments):
+		print comment.body
+
+	print("Bot work done")
+
+def main():
+	reddit = authenticate()
+	run_bot(reddit)
+
+if __name__ == "__main__":
+	main()
